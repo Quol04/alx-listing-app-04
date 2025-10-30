649ca2dd8b3605b73895bb699ba7d9daa60c370b
quol <kuolamol77@gmail.com>
2025-10-30 13:18:37 +0300

    added the initial setup

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -32,27 +32,3 @@ export interface ButtonProps {
   action?: () => void
 }
 
-//     name: string,
-//     address: Address,
-//     rating: number,
-//     category: Category,
-//     price: number,
-//     offers: Offers,
-//     image:string ,
-//     discount: string,
-// }
-
-// export interface Address {
-//       state: string,
-//       city: string,
-//       country: string,
-// }
-// export interface Category{
-//     // ["Luxury Villa", "Pool", "Free Parking"]
-// }
-
-// export interface Offers{
-//     bed: string,
-//     shower: string,
-//     occupants: string,
-
